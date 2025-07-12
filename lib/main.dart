6f247c75c0d9795031cda4cfb657c30a66d2808f
ipek-ozsasilar <ipek.ozsslr@gmail.com>
2025-07-12 20:58:37 +0300

    Integrated responsive framework for adaptive UI layout in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_e_commerce_app/gen/assets.gen.dart';
 import 'package:flutter_e_commerce_app/gen/colors.gen.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,8 +18,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+       builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+           const Breakpoint(start: 0, end: 450, name: MOBILE),
+           const Breakpoint(start: 451, end: 800, name: TABLET),
+           const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+           const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
       home: const MyHomePage(),
     );
+  
   }
 }
 
